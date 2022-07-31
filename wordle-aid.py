9a2e7470ccd5d51e0be1e909ffdcf076fd64d560
Dennis Henley <radishmintmonkey@gmail.com>
2022-07-31 16:35:04 -0500

    started on main loop and menu function

--- wordle-aid.py
+++ wordle-aid.py
@@ -4,6 +4,25 @@ import re
 from wordle_list import wordle_list
 from wordle_list import wordle_string
 
+# Program
+# Loop until quit
+# Make choices from menu
+# Menu Content
+# Quit
+# Display word list
+# Reduce list to only words with RegEx Matches
+# Reduce list by removing all words that use eliminated letters
+
+# Ask for located letters and reduce word list to only RegEx matches
+# Keep track of position of located letters so that position can be skipped
+# when reducing by eliminated letters
+# Ask for eliminated letters and reduce word list by removing words that
+# contain eliminated letters
+# Loop until guesses > 6 or word is revealed.
+
+
+
+
 response = input("Did your guess contain any letters used that were \nin the correct location? (Y/N)   ").lower()
 
 if response == "y":
@@ -17,12 +36,12 @@ if response == "y":
     regex_str = input("\nEnter the RegEx pattern to match (?????):\n").upper()
     regex_str += ','
 
-search_results = re.findall(regex_str, wordle_string.upper())
-new_string = ""
-if search_results:
-  for item in search_results:
-    new_string += item + " "
+    search_results = re.findall(regex_str, wordle_string.upper())
+    new_string = ""
+    if search_results:
+      for item in search_results:
+        new_string += item + " "
+      print(new_string)
 else:
   print("No match")
 
-print(new_string)
